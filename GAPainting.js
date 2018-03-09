b94765eee882f0f654b0dd13d410a8ea8ec9886a
Jingyi Yuan <jingyy3@cs.washington.edu>
2018-03-08 23:22:19 -0800

    add individual

--- GAPainting.js
+++ GAPainting.js
@@ -3,11 +3,29 @@ var canvasRes = document.getElementById('resultImg');
 var contextOri = canvasOri.getContext('2d');
 var contextRes = canvasRes.getContext('2d');
 
+contextCur --- workingContext
+canvasCur --- workingCanvas
+
 var populationSize;
 var mutationChance;
 var mutationAmount;
 var selectionCutoff;
 var polygonNumber;
+
+//speficy resolution (workingSize)
+var resolution = 75;
+
+//specify the number of vertices, which is now initialized to be 
+var vertices = 3;
+
+//initialized in the init 
+var geneLen;
+//hard coded
+var geneSize = 4  + vertices * 2;
+
+
+
+
 
 window.onload = function(){
 	make_base();
@@ -32,7 +50,126 @@ function Evolve(){
 	resImage.onload = function(){
     	contextRes.drawImage(resImage, 0, 0);
   }
+
+
 };
+
+
+/* Each individual represents a single gene in the gene pool.
+Randomly decide in the Population if it's inherited from both parents or randomly generated */
+function Individual(randInt, father, mother){
+	this.gene = [];
+
+	if(randInt == 0){
+		//randomly generate a new gene
+		for(var i = 0; i < geneLen; i += geneSize ){
+			//RGBA
+			this.gene.push(Math.random(),  Math.random(), Math.random(), Math.max(Math.random() * Math.random(), 0.4));
+
+			var xOffset = Math.random();
+			var yOffset = Math.random();
+
+			//randomly generate positions for each vertex of the polygon
+			for(var j = 0; j < verices; j++){
+				//randomly generate x, y pos for each of the vertex
+				this.gene.push(xOffset + Math.random() - 0.6, yOffset + Math.random() - 0.6);
+			}
+		}
+
+	}else{
+		//inherit from both parents
+		var inheritSplit = (Math.random() * geneLen) >> 0;
+
+		for (var i = 0; i < geneLen; i += geneSize) {
+
+			/* The parent's gene which will be inherited */
+			var inheritedGene;
+	
+			if (randomInheritance) {
+			  /* Randomly inherit genes from parents in an uneven manner */
+			  inheritedGene = (i < inheritSplit) ? father : mother;
+			} else {
+			  /* Inherit genes evenly from both parents */
+			  inheritedGene = (Math.random() < 0.5) ? father : mother;
+			}
+	
+			
+			for (var j = 0; j < geneSize; j++) {
+	
+			  var singleGene = inheritedGene[i + j];
+	
+			  if (Math.random() < mutationChance) {
+	
+				/* Apply the random mutation */
+				singleGene += Math.random() * mutatationAmount * 2 - mutatationAmount;
+	
+				/* Keep the value in range */
+				if (singleGene < 0)
+					singleGene = 0;
+	
+				if (singleGene > 1)
+				  	singleGene = 1;
+			  }
+			  this.gene.push(singleGene);
+			}
+		  }
+	}
+
+	drawPolygon(contextCur,resolution, resolution, this);
+
+
+	var imgData = getImageData(0, 0, resolution, resolution).data;
+	var diff = 0;
+
+
+
+
+	//get the fitness value for the gene
+	//sum differences
+	for(var i = 0; i < 4 *  resolution * resolution; i++){
+		diff += Math.abs(imageData[i] - workingData[i]);
+	}
+
+	this.fitness = 1 - diff / (resolution * resolution * 256 * 4);
+}
+
+
+function drawPolygon(context, width, height, individual){
+
+	context.fillStyle = '#000';
+	context.fillRect(0, 0, width, height);
+	//draw gene sequentially
+	for(var i = 0; i < geneLen; i += geneSize){
+
+		//starting vertex
+		context.beginPath();
+
+		//Could tune some parameters here
+		context.moveTo(individual.gene[i + 4] * width, individual.gene[i + 5] * height);
+
+		//draw each vertices
+		for(var j = 0; j < vertices - 1; j++){
+			context.lineTo(width * individual.gene[i + j * 2 + 6], height * individual.gene[i + j * 2 + 7]);
+		}
+
+		context.closePath();
+
+		var styleString = 'rgba(' +
+		((individual.gene[i] * 255) >> 0) + ',' + 
+		((individual.gene[i + 1] * 255) >> 0) + ',' + 
+		((individual.gene[i + 2] * 255) >> 0) + ',' + 
+		individual.gene[i + 3] + ')'; 
+
+		//create a polygon
+		ctx.fillStyle = styleString;
+		ctx.fill();
+		
+  }
+
+}
+
+
+
 
 
 function make_base()
@@ -74,8 +211,11 @@ function init(){
 	}
 	document.getElementById("polygon").oninput = function(){
 		polygonNumber = event.srcElement.value;	
+		geneLen = polygonNumber * (4 + vertices * 2);
 		val5.innerHTML = polygonNumber;
 	}
+
+	
 }
 
 
